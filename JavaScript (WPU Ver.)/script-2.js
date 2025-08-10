7af4914f79e03a9ede1d5bb00d1468defd215b0f
inouerakan <rakanprojek@gmail.com>
2025-08-10 16:28:58 +0700

    Belajar JavaScript - 4

---
+++ JavaScript (WPU Ver.)/script-2.js
@@ -0,0 +1,14 @@
+console.log(9)
+console.log(999999999999999) // 15 digits
+console.log(9999999999999999) // 16 digits
+console.log(0.3 + 0.7)
+console.log(0.2 + 0.1)
+console.log(123e5)
+console.log(123e-5)
+console.log(022) // octal
+console.log(0x22) // hex
+console.log(2 / 0)
+console.log(-2 / 0)
+console.log(0 / 0)
+console.log(100 / 'Hello')
+console.log(100 / '50') // Javascript will treat the string as a number
