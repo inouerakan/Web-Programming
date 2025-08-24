57e828f7f77c2e46f2a7fad6579b10eba394c1d5
inouerakan <rakanprojek@gmail.com>
2025-08-24 19:59:16 +0700

    Belajar JavaScript - 8

--- JavaScript (WPU Ver.)/script-6(Arrow Function).js
+++ JavaScript (WPU Ver.)/script-6(Arrow Function).js
@@ -31,7 +31,7 @@
 
 
 
-let member = ['Inoue Nagi','Masuda Mirine','Setoguchi Mitsuki'];
+// let member = ['Inoue Nagi','Masuda Mirine','Setoguchi Mitsuki'];
 
 // let jumlah_huruf = member.map(function (nama) {
 //     return nama.length;
@@ -39,6 +39,77 @@ let member = ['Inoue Nagi','Masuda Mirine','Setoguchi Mitsuki'];
 
 // let jumlah_huruf = member.map(nama => nama.length)
 
-let jumlah_huruf = member.map(nama => ({nama: nama, jumlah_huruf: nama.length}))
+// let jumlah_huruf = member.map(nama => ({nama: nama, jumlah_huruf: nama.length}))
 
-console.table(jumlah_huruf)
+// console.table(jumlah_huruf)
+
+
+
+// Konsep this pada arrow function
+// Constructor function
+// const Member = function() {
+//     this.nama = 'Nagi';
+//     this.umur = 20;
+//     this.halo = function() {
+//         console.log(`Halo nama saya, ${this.nama}. Saya ${this.umur} tahun.`);
+//     }
+// }
+
+// const nagi = new Member();
+
+// Arrow function
+// const Member = function() {
+//     this.nama = 'Nagi';
+//     this.umur = 20;
+//     this.halo = () => {
+//         console.log(`Halo nama saya, ${this.nama}. Saya ${this.umur} tahun.`);
+//     }
+// }
+
+// const nagi = new Member();
+// nagi.halo()
+
+// Object literal
+// const nagi = {
+//     nama: 'Inoue Nagi',
+//     umur: 20,
+//     halo: () => {console.log(`Halo nama saya, ${this.nama}. Saya ${this.umur} tahun`);} // Akan undefined karena arrow function tidak memiliki konsep this
+// }
+
+// nagi.halo()
+
+
+
+// Kasus lain di constructor function
+// const Member = function() {
+//     this.nama = 'Nagi';
+//     this.umur = 20;
+//     this.halo = function() {
+//         console.log(`Halo nama saya, ${this.nama}. Saya ${this.umur} tahun.`);
+//     }
+
+//     setInterval(function() {
+//         console.log(this)
+//     }, 500)
+
+//     setInterval(() => {
+//         console.log(this);
+//     }, 500);
+// }
+
+// const nagi = new Member();
+
+
+
+const box = document.querySelector('.box');
+box.addEventListener('click', function() {
+    let satu = 'size'
+    let dua = 'caption'
+    if (this.classList.contains(satu)) {
+        [satu,dua] = [dua,satu];
+    }
+    this.classList.toggle(satu);
+    setTimeout(() => {
+        this.classList.toggle(dua);
+    }, 600)
+})
