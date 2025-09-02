1abfc609ccd7c375ee38bc4bafc985fc7949ea29
inouerakan <rakanprojek@gmail.com>
2025-09-02 17:30:21 +0700

    Belajar JavaScript - 11

---
+++ JavaScript (WPU Ver.)/script-9(latihan script-8).js
@@ -0,0 +1,13 @@
+// Ambil semua elemen video
+
+// Pilih hanya yang "Music Video"
+
+// Ambil durasi masing-masing video
+
+// Ubah durasi menjadi integer, ubah menit jadi detik
+
+// Jumlahkan semua detiknya
+
+// Ubah formatnya jadi jam menit detik
+
+// Simpan di DOM
